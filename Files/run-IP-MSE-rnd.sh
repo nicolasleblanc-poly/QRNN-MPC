b5f501956f8ae558fd3222f3687d120052ca86c5
Nicolas Leblanc <nileb3@cedar5.int.cedar.computecanada.ca>
2025-05-14 04:37:17 -0700

    hjbhjb

--- Files/run-IP-MSE-rnd.sh
+++ Files/run-IP-MSE-rnd.sh
@@ -12,7 +12,7 @@ module purge
 module load python/3.11.9 scipy-stack
 source ~/myenv/bin/activate
 
-python run_IP_Q_rnd_RS.py # my_script.py
+python run_IP_MSE_rnd.py # my_script.py
 # echo "all done"
 
 # #!/bin/bash
