969206e04f5bc4e113283ed262d578845018e1fc
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2025-05-16 19:22:16 -0400

    3ee

--- mppi/run_PE_mppi.sh
+++ mppi/run_PE_mppi.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=96:0:0    
+#SBATCH --mail-user=<nicolas-3.leblanc@polymtl.ca>
+#SBATCH --mail-type=ALL
+
+# cd ~/$projects/def-bonizzat/nileb3/job-scripts/
+# python /home/nileb3/projects/def-bonizzat/nileb3/job-scripts/my_script.py
+module purge
+module load mujoco
+module load python/3.10.13 scipy-stack
+source ~/myenv/bin/activate
+
+python Pendulum_approximate_continuous.py # my_script.py
+
 # #!/bin/bash
 # #SBATCH --job-name=mountaincar-gpu
 # #SBATCH --mem=32G
@@ -23,19 +40,3 @@
 # # Run your Python script
 # python Pendulum_approximate_continuous.py # my_script.py
 
-#!/bin/bash
-#SBATCH --mem=32G
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
-#SBATCH --time=96:0:0    
-#SBATCH --mail-user=<nicolas-3.leblanc@polymtl.ca>
-#SBATCH --mail-type=ALL
-
-# cd ~/$projects/def-bonizzat/nileb3/job-scripts/
-# python /home/nileb3/projects/def-bonizzat/nileb3/job-scripts/my_script.py
-module purge
-module load mujoco
-module load python/3.10.13 scipy-stack
-source ~/myenv/bin/activate
-
-python Pendulum_approximate_continuous.py # my_script.py
