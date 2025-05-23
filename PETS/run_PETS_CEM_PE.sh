10dbb1181160384f6358fbc25a28e679f1570e04
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2025-05-22 20:58:45 -0400

    3r3

--- PETS/run_PETS_CEM_PE.sh
+++ PETS/run_PETS_CEM_PE.sh
@@ -10,7 +10,9 @@
 # python /home/nileb3/projects/def-bonizzat/nileb3/job-scripts/my_script.py
 module purge
 module load mujoco
-module load python/3.10.0 scipy-stack/2023a
-source ~/py310new/bin/activate
+# module load python/3.10.0 scipy-stack/2023a
+# source ~/py310new/bin/activate
+module load python/3.11.9 scipy-stack
+source ~/myenv/bin/activate
 
 python main_PETS_CEM_PE.py # my_script.py
