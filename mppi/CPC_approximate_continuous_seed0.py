5159a115962cb1650053027a074c16091d12cefe
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2025-06-02 13:44:36 -0400

    e2e

--- mppi/CPC_approximate_continuous_seed0.py
+++ mppi/CPC_approximate_continuous_seed0.py
@@ -241,7 +241,7 @@ if __name__ == "__main__":
     episodic_return_seeds = []
     max_episodes = 300
     method_name = "MPPI"
-    prob = "InvertedPendulum"
+    prob = "CPC"
     max_steps = 1000
     
     # for seed in env_seeds:
