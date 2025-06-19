442991712494add50d4530786c734393cf98a050
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2025-06-18 20:17:33 -0400

    23e

--- Files/setup.py
+++ Files/setup.py
@@ -754,7 +754,7 @@ class setup_class:
                     torch.zeros_like(side_thrust)
                 )
                 
-                position_cost = 100*np.sqrt(x**2 + y**2)  # Penalize distance from the origin
+                position_cost = 100*torch.sqrt(x**2 + y**2)  # Penalize distance from the origin
                 velocity_cost = 0.1 * (vx**2 + vy**2)  # Penalize high velocities
                 angle_cost = 10 * theta**2  # Penalize deviation from upright position
                 contact_bonus = -10 * (leg1 + leg2)  # Reward for having legs in contact with the ground
