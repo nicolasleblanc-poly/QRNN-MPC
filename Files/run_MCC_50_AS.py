346359d95e35e0fba810df2702fa34ac0972eb39
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2025-06-23 15:04:33 -0400

    qwd

--- Files/run_MCC_50_AS.py
+++ Files/run_MCC_50_AS.py
@@ -40,7 +40,7 @@ def save_data(prob, method_name, episodic_rep_returns, mean_episodic_returns, st
     # }
 
     np.savez(
-    f"{prob}_{method_name}_May6.npz",
+    f"{prob}_{method_name}_June21.npz",
     episode_rewards=episodic_rep_returns,
     mean_rewards=mean_episodic_returns,
     std_rewards=std_episodic_returns
