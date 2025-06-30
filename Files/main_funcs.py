b9e42708e154280f6ba9b820de5219e692dac4c0
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2025-06-30 14:13:35 -0400

    4r3r

--- Files/main_funcs.py
+++ Files/main_funcs.py
@@ -151,7 +151,7 @@ def main_QRNN_MPC(prob_vars, method_name, model_QRNN, replay_buffer_QRNN, optimi
         https://gymnasium.farama.org/main/_modules/gymnasium/wrappers/record_video/
         """
         
-        if prob_vars.prob != "MuJoCoReacher" and prob_vars.prob != "MuJoCoPusher":
+        if prob_vars.prob != "MuJoCoReacher" and prob_vars.prob != "MuJoCoPusher" and prob_vars.prob != "InvertedPendulum":
             if prob_vars.change_prob is None and prob_vars.std is not None:
                 env = RecordVideo(prob_vars.env, video_folder="videos", episode_trigger=should_record, name_prefix=f"{prob_vars.prob}_{prob_vars.std}_{method_name}_seed{seed}") # , video_length=prob_vars.max_steps
             elif prob_vars.change_prob is not None and prob_vars.std is None:
