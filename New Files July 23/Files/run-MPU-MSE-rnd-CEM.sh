b0826181e2063d6b4d935cbbaf089dba29171eea
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2025-07-23 15:06:46 -0400

    2e

---
+++ New Files July 23/Files/run-MPU-MSE-rnd-CEM.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=96:0:0    
+#SBATCH --mail-user=<nicolas-3.leblanc@polymtl.ca>
+#SBATCH --mail-type=ALL
+
+# cd ~/$projects/def-bonizzat/nileb3/job-scripts/
+# python /home/nileb3/projects/def-bonizzat/nileb3/job-scripts/my_script.py
+module purge
+module load python/3.11.9 scipy-stack
+source ~/myenv/bin/activate
+
+python run_MPU_MSE_AS_ba_CEM.py # my_script.py
+# echo "all done"
+
+# #!/bin/bash
+# #SBATCH --mem=32G
+# #SBATCH --nodes=1
+# #SBATCH --ntasks-per-node=8
+# #SBATCH --time=168:0:0    
+# #SBATCH --mail-user=<nicolas-3.leblanc@polymtl.ca>
+# #SBATCH --mail-type=ALL
+
+# # cd ~/$projects/def-bonizzat/nileb3/job-scripts/
+# # python /home/nileb3/projects/def-bonizzat/nileb3/job-scripts/my_script.py
+# module purge
+# module load python/3.11.9 scipy-stack
+# source ~/myenv/bin/activate
+
+# # python run.py # my_script.py
+# echo "starting..."
+# python -c "import torch; print('CUDA AVAILABLE: ' + str(torch.cuda.is_available()))"
+# echo "all done"
+
+
+# # #!/bin/bash
+# # #SBATCH --time=10:00:00	    # Requests the amount of time needed for the job.
+# # #SBATCH --nodes=1	    # Number of nodes to request. (Default=1).
+# # #SBATCH --ntasks=1 # Number of threads/MPI process.
+# # #SBATCH --gres=gpu:1              # Number of GPU(s) per node
+# # #SBATCH --cpus-per-task=6         # CPU cores/threads/tasks
+# # #SBATCH --mem=16000M               # memory per node (0 means entire memory)
+
+# # module load python/3.11
+# # module load scipy-stack
+# # source ~/myenv/bin/activate
+# # echo "starting..."
+# # python -c "import torch; print('CUDA AVAILABLE: ' + str(torch.cuda.is_available()))"
+# # echo "all done"
+
+
