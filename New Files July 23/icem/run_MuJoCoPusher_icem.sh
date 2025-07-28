81930fbabfbea0a5ae35e5c369615a6577dae0c9
Nic Leblanc <106348280+nicolasleblanc-poly@users.noreply.github.com>
2025-07-28 09:35:06 -0400

    Update run_MuJoCoPusher_icem.sh

--- New Files July 23/icem/run_MuJoCoPusher_icem.sh
+++ New Files July 23/icem/run_MuJoCoPusher_icem.sh
@@ -15,7 +15,7 @@ module load mujoco
 module load python/3.11.9 scipy-stack
 source ~/myenv/bin/activate
 
-python MuJoCoReacher_approximate_continuous.py # my_script.py
+python MuJoCoPusher_approximate_continuous.py # my_script.py
 
 # #!/bin/bash
 # #SBATCH --mem=32G
